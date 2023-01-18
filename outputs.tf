358f045eec196f73b0c67007f86b3ccfc063635f
Garland Kan <garland.kan@MacBook-Pro.local>
2023-01-18 13:39:29 -0800

    fixing output name

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@
 #   value = azurerm_role_definition.example.role_definition_id
 # }
 
-output "azuread_application.app.application_id" {
+output "azuread_application_app_application_id" {
     value = azuread_application.app.application_id
 }
 
-output "azuread_service_principal.app.id" {
+output "azuread_service_principal_app_id" {
     value = azuread_service_principal.app.id
 }
