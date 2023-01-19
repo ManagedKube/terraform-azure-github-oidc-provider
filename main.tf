0dce0c3c663c26417b700dceae1bb3f8d6a8f493
Garland Kan <garland.kan@MacBook-Pro.local>
2023-01-19 14:35:35 -0800

    removing unused commented out code

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 locals {
-#   helm_repository     = "https://kubernetes-sigs.github.io/external-dns/"
-#   official_chart_name = "external-dns"
   base_name           = "github-actions-oidc"
   ## This should match the name of the service account created by helm chart
   service_account_name = "${local.base_name}-${var.environment_name}"
@@ -28,21 +26,6 @@ resource "azuread_application" "app" {
   required_resource_access {
     resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
 
-    # resource_access {
-    #   id   = azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
-    #   type = "Role"
-    # }
-
-    # resource_access {
-    #   id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.ReadWrite"]
-    # #   type = "Scope"
-    #   type = "Role"
-    # }
-
-    # resource_access {
-    #   id   = azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
-    #   type = "Scope"
-    # }
     resource_access {
       id   = azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
       type = "Role"
@@ -74,40 +57,7 @@ resource "azuread_application_federated_identity_credential" "app" {
   issuer                = "https://token.actions.githubusercontent.com" # var.oidc_k8s_issuer_url
   # Doc for the `subject` string: https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/about-security-hardening-with-openid-connect#configuring-the-subject-in-your-cloud-provider
   subject               = "repo:intacct/do-infrastructure:pull_request"
-    #"repo:octo-org/octo-repo:ref:refs/heads/demo-branch"
-    #"system:serviceaccount:${var.k8s_namespace}:${local.service_account_name}"
 }
-
-## Role assignment to the application
-## Doc: https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/azure.md#assign-the-rights-for-the-service-principal
-## Access permissions
-## Error: authorization.RoleAssignmentsClient#Create: Failure
-## This most likely means you do not have the correct permissions to perform a role assignment
-## Doc: https://learn.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal
-## This doc shows how to perform a role assignment and talks about checking the permission you have
-## Your role might be a global admin but you also need the correct permissions for the Azure Subscription
-## This doc shows you how to check what permissions you have for a subscription
-## Check: Azure portal -> Subscriptions -> <the subscription in question> -> Settings -> My Permissions -> Go to subscription access control (IAM) -> Check Access -> View My access
-## The "contributor" role is not enough.  Per the doc you need: Owner role or User Access Administrator role
-##
-## The assigned role external-dns needs:
-## * https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/azure-private-dns.md#configure-service-principal-for-managing-the-zone
-## * role name: "Private DNS Zone Contributor"
-##
-## Checking the role assignment:
-## * Azure portal -> Azure DNS -> <the zone in question> -> Access Control (IAM) -> Role Assignment
-## * The "external-dns-<env>" service principal should be in this list
-##
-# resource "azurerm_role_assignment" "app_storage_contributor" {
-#   ## Scope to for assignment
-#   scope                = azuread_application.app.object_id
-#   ## The role name (pre-defined built in azure roles)
-#   ## Use the "role_definition_id" var to provide it with a custom role definition (see terraform doc for this resource for more informations)
-#   role_definition_name = var.role_definition_name
-#   ## The principal to assign it to
-#   principal_id         = azuread_service_principal.app.id
-# }
-
 
 data "azurerm_subscription" "primary" {
 }
@@ -115,56 +65,6 @@ data "azurerm_subscription" "primary" {
 resource "azurerm_role_assignment" "this" {
 #   name                 = azurerm_role_definition.example.role_definition_id
   scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Contributor" # "Owner"
+  role_definition_name = var.role_definition_name
   principal_id         = azuread_service_principal.app.id
 }
-
-# resource "azurerm_role_assignment" "graph_assignment" {
-# #   name                 = azurerm_role_definition.example.role_definition_id
-#   scope                = data.azurerm_subscription.primary.id
-#   role_definition_name = "Application.ReadWrite.All" #"Owner"
-#   principal_id         = azuread_service_principal.app.id
-# }
-
-# data "azuread_application_published_app_ids" "well_known" {}
-
-# resource "azuread_service_principal" "msgraph" {
-#   application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
-#   use_existing   = true
-# }
-
-# resource "azuread_app_role_assignment" "msgraph_role" {
-# #   app_role_id         = azuread_service_principal.msgraph.app_role_ids["Application.ReadWrite.All"]
-# #   principal_object_id = azuread_service_principal.example.object_id
-# #   resource_object_id  = azuread_service_principal.msgraph.object_id
-
-#   app_role_id         = azuread_service_principal.app.app_role_ids["User.Read.All"]
-#   principal_object_id = azuread_service_principal.app.object_id
-#   resource_object_id  = azuread_service_principal.msgraph.object_id
-# }
-
-# Application.ReadWrite.All
-
-# resource "azurerm_role_definition" "example" {
-# #   role_definition_id = "00000000-0000-0000-0000-000000000000"
-#   name               = "my-custom-role-definition"
-#   scope              = data.azurerm_subscription.primary.id
-
-#   permissions {
-#     # See the Terraform resource azurerm_role_definition for more info on
-#     # what "actions" are possible
-#     actions     = ["*"]
-#     not_actions = []
-#   }
-
-#   assignable_scopes = [
-#     data.azurerm_subscription.primary.id,
-#   ]
-# }
-
-# resource "azurerm_role_assignment" "example" {
-#   name               = azurerm_role_definition.example.role_definition_id
-#   scope              = data.azurerm_subscription.primary.id
-#   role_definition_id = azurerm_role_definition.example.role_definition_resource_id
-#   principal_id       = azuread_service_principal.app.id
-# }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-# output "azurerm_role_definition_role_definition_id" {
-#   value = azurerm_role_definition.example.role_definition_id
-# }
-
 output "azuread_application_app_application_id" {
     value = azuread_application.app.application_id
 }
